9f887c14f47be9caf8c8ecc88253b0c91e1ff92e
pramodsharma92 <pramod.a.sharma46@gmail.com>
2021-12-20 18:51:33 +0530

    sample gradle project

---
+++ testsonar/src/main/java/com/example/testsonar/TestsonarApplication.java
@@ -0,0 +1,13 @@
+package com.example.testsonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsonarApplication.class, args);
+	}
+
+}
